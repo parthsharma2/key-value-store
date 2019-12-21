cbe97b78c231eb57e37714f7f4559770980e442e
Parth Sharma <parth261297@gmail.com>
2019-12-21 19:34:21 +0530

    improve client command execution
    Makes the command execution more robust. Handle command
    incorrect usage.
    
    Signed-off-by: Parth Sharma <parth261297@gmail.com>

--- kvs/client.py
+++ kvs/client.py
@@ -10,7 +10,7 @@ class Client:
 
     def __init__(self, host='127.0.0.1', port=25552):
         """
-        Creates a client to connect to the KeyValueStore REST Server.
+        Create a client to connect to the KeyValueStore REST Server.
 
         :param host: The host address to connect to, defaults to 127.0.0.1
         :type host: str, optional
@@ -23,37 +23,67 @@ class Client:
         self.port = port
         self.url = f'http://{host}:{port}/api'
 
-        # Check if the REST server is running
         try:
+            # Check if the REST API server is running
             requests.get(f'{self.url}/ping')
         except requests.exceptions.ConnectionError:
+            # Chances are the REST API server is not
+            # running or host/port is incorrect
             print(f"Couldn't connect to {self.host}:{self.port}!",
                   "Make sure the KeyValueStore REST server is running.")
             self.exit(1)
         except Exception as e:
+            # Unexpected Error
             logger.exception(e)
             self.exit(1)
 
     def execute(self, cmd, *args):
-        """Execute a command"""
-        if cmd.lower() == 'get':
-            response = requests.get(f'{self.url}/{args[0]}')
+        """Execute a command
 
-            if 'result' in response.json():
+        :param cmd: Command to be executed
+        :type cmd: str
+        :return: Result of the command
+        :rtype: str
+        """
+        if cmd.lower() == 'get':
+            if len(args) != 1:
+                return 'get: Incorrect usage! Expected 1 argument'
+
+            key = args[0]
+            response = requests.get(f'{self.url}/{key}')
+
+            if response.status_code == 200  and 'result' in response.json():
                 return response.json()['result']
+            else:
+                logger.error(f'{response.status_code}: {response.text}')
+                return 'Unexpected Error!'
 
         elif cmd.lower() == 'set':
-            response = requests.post(f'{self.url}/{args[0]}',
-                                     json={'value': args[1]})
+            if len(args) != 2:
+                return 'set: Incorrect usage! Expected 2 arguments'
 
-            if 'result' in response.json():
+            key, value = args[0], args[1]
+            response = requests.post(f'{self.url}/{key}',
+                                     json={'value': value})
+
+            if response.status_code == 200  and 'result' in response.json():
                 return response.json()['result']
+            else:
+                logger.error(f'{response.status_code}: {response.text}')
+                return 'Unexpected Error!'
 
         elif cmd.lower() == 'delete':
-            response = requests.delete(f'{self.url}/{args[0]}')
+            if len(args) != 1:
+                return 'delete: Incorrect usage! Expected 1 argument'
 
-            if 'result' in response.json():
+            key = args[0]
+            response = requests.delete(f'{self.url}/{key}')
+
+            if response.status_code == 200 and 'result' in response.json():
                 return response.json()['result']
+            else:
+                logger.error(f'{response.status_code}: {response.text}')
+                return 'Unexpected Error!'
 
         elif cmd.lower() == 'exit':
             self.exit(0)
@@ -62,7 +92,7 @@ class Client:
             return 'Unrecognized command!'
 
     def io_loop(self):
-        """Begins the IO loop allowing you to send/receive data to and from the server."""
+        """Run the IO loop to receive input from the user and execute it."""
         while True:
             try:
                 inp = input('> ')
