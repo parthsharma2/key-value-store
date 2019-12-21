e1b6d5452d7846b60de9248e0e40aa7fd7b27ee4
Parth Sharma <parth261297@gmail.com>
2019-12-21 13:30:53 +0530

    convert server.py to a REST server
    Signed-off-by: Parth Sharma <parth261297@gmail.com>

--- kvs/server.py
+++ kvs/server.py
@@ -1,140 +1,49 @@
-import logging
-import socket
 import threading
+from flask import Flask, request, jsonify, abort
 
 from kvs.kvs import KeyValueStore
 
 
-logger = logging.getLogger(__name__)
-logging.basicConfig(level=logging.INFO)
+app = Flask(__name__)
+key_value_store = KeyValueStore()
+lock = threading.RLock()
 
-class Server:
+@app.route('/api/<key>', methods=['GET'])
+def get(key):
+    with lock:
+        result = key_value_store.get(key)
 
-    def __init__(self, host='127.0.0.1', port=25552, max_clients=4, size=1024):
-        """Creates a server instance of KeyValueStore
+    result = result if result else 'null'
+    return jsonify({'result': result})
 
-        :param host: The host address to run the server on, defaults to '127.0.0.1'
-        :type host: str, optional
-        :param port: The port to run the server on, defaults to 25552
-        :type port: int, optional
-        :param max_clients: The max number of clients to accept, defaults to 4
-        :type max_clients: int, optional
-        :param size: The buffer size to receive data
-        :type size: int, optional
-        """
-        self.kv = KeyValueStore()
-        self.host = host
-        self.port = port
-        self.max_clients = max_clients
-        self.size = size
-        self.lock = threading.RLock()
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.sock.bind((host, port))
+@app.route('/api/<key>', methods=['POST'])
+def set(key):
+    if not request.json or not 'value' in request.json:
+        abort(400)
 
-    def listen(self):
-        """Begins the servers' listening process."""
-        self.sock.listen(self.max_clients)
-        logger.info(f'Server listening on {self.host}:{self.port}')
+    val = request.json.get('value')
 
-        while True:
-            client, address = self.sock.accept()
-            threading.Thread(target=self.handle_client,
-                             args=(client, address)).start()
+    with lock:
+        result = key_value_store.set(key, val)
 
-    def handle_client(self, client, address):
-        """Handles a client
+    if result:
+        result = f'{key}:{val} stored successfully'
+    else:
+        result = f'{key}:{val} error storing'
 
-        :param client: A socket object representing the
-        :type client: `socket.socket`
-        :param address: Address of the client
-        :type address: tuple
-        """
-        logger.info(f'Connection Received: {address[0]}:{address[1]}')
-        while True:
-            try:
-                data = client.recv(self.size)
-                if data:
-                    response = self.handle_request(data)
-                    client.sendall(response.encode())
-                else:
-                    logger.info(f'Client {address[0]}:{address[1]} disconnected')
-                    client.close()
-            except Exception:
-                client.close()
-                return
+    return jsonify({'result': result})
 
-    def handle_request(self, data):
-        """Handles a request from the client
+@app.route('/api/<key>', methods=['DELETE'])
+def delete(key):
+    with lock:
+        result = key_value_store.delete(key)
 
-        :param data: Data representing the request
-        :type data: bytes
-        :return: A response to the request
-        :rtype: str
-        """
-        self.lock.acquire()
+    if result:
+        result = f'{key} deleted'
+    else:
+        result = f'{key} does not exist'
 
-        data = data.decode()
-        response = self.execute(*data.split())
-
-        self.lock.release()
-        return response
-
-    def execute(self, cmd, *args):
-        """Executes the command specified by cmd
-
-        :param cmd: The command to be executed
-        :type cmd: str
-        :return: The result of the command
-        :rtype: str
-        """
-        if cmd.lower() == 'get':
-            if len(args) != 1:
-                return 'GET: Incorrect Usage! Specify 1 argument'
-
-            resp = self.kv.get(args[0])
-            if resp:
-                return resp
-            else:
-                return 'null'
-
-        elif cmd.lower() == 'set':
-            if len(args) != 2:
-                return 'SET: Incorrect Usage! Specify 2 arguments'
-
-            resp = self.kv.set(args[0], args[1])
-            if resp:
-                return f'{args[0]}:{args[1]} stored successfully'
-            else:
-                return f'{args[0]}:{args[1]} error storing'
-
-        elif cmd.lower() == 'delete':
-            if len(args) != 1:
-                return 'DELETE: Incorrect Usage! Specify 1 argument'
-            resp = self.kv.delete(args[0])
-            if resp:
-                return f'{args[0]} deleted'
-            else:
-                return f'{args[0]} does not exist'
-
-        else:
-            return 'Unrecognized Command'
-
-    def close(self):
-        """Closes the server."""
-        # TODO Handle close server when clients are still connected
-        logger.info('Closing Server')
-        self.sock.close()
-
-def main():
-    server = Server()
-
-    try:
-        server.listen()
-    except (KeyboardInterrupt, EOFError):
-        server.close()
-    except Exception as e:
-        logger.exception(e)
-        server.close()
+    return jsonify({'result': result})
 
 if __name__ == "__main__":
-    main()
+    app.run(debug=True)
