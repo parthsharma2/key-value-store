b78c42da19f888b7942e96c78f69c788b5c1c175
Parth Sharma <parth261297@gmail.com>
2019-12-21 19:55:47 +0530

    refactor kvs.py
    - Add exception handling to `set`
    - Improve docstrings
    
    Signed-off-by: Parth Sharma <parth261297@gmail.com>

--- kvs/kvs.py
+++ kvs/kvs.py
@@ -8,16 +8,32 @@ logger = logging.getLogger(__name__)
 class KeyValueStore(metaclass=Singleton):
 
     def __init__(self):
+        """Key Value Store capable of getting/setting/deleting key-value pairs."""
         self.dictionary = {}
 
     def set(self, key, value):
-        """Sets a key to a particular value"""
-        self.dictionary[key] = value
-        logger.info(f'SET {key}: {value}')
-        return True
+        """Set a key to a particular value.
+
+        :param key: The key to set the value to.
+        :param value: The value to assign to the key.
+        :return: True if success, False if failure
+        :rtype: bool
+        """
+        try:
+            self.dictionary[key] = value
+            logger.info(f'SET {key}: {value}')
+            return True
+        except Exception as e:
+            logger.info(f'SET {key}: {value} failed')
+            logger.exception(e)
+            return False
 
     def get(self, key):
-        """Fetch the data stored at the given key"""
+        """Fetch the data stored at the given key.
+
+        :param key: The key to fetch the value of.
+        :return: Value of the key, if it exists, else None
+        """
         if key in self.dictionary:
             logger.info(f'GET {key}: success')
             return self.dictionary[key]
@@ -26,7 +42,12 @@ class KeyValueStore(metaclass=Singleton):
             return None
 
     def delete(self, key):
-        """Delete the value stored at the given key"""
+        """Delete the value stored at the given key.
+
+        :param key: The key to delete.
+        :return: True if key exists and deleted else False
+        :rtype: bool
+        """
         if key in self.dictionary:
             del self.dictionary[key]
             logger.info(f'DELETE {key}: success')
